5b01b86c7f4e08ac26c8d6eec809a197f759bd2a
Fawwaz <fwwz.id@gmail.com>
2025-02-25 22:21:46 +0700

    Revert "feat/prototype: simple prototype of the app"

--- src/app/_provider/ThemeProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
-import theme from "../theme";
-
-export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-    return <MUIThemeProvider theme={theme}>
-        {children}
-    </MUIThemeProvider>
-}
